f1e0c0832b8ed377c39287d7e5a8be0dba741736
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-09-13 09:46:56 -0600

    fix en el reporte de aprobados y reprobados

--- skycom/views.py
+++ skycom/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import viewsets
-from django.db.models import Avg
+from django.db.models import Avg,Count
 from .serializer import ProfesorSerializer, AlumnoSerializer, MateriaSerializer, NotaSerializer
 from .models import Profesor, Alumno, Materia, Nota
 
@@ -25,29 +25,36 @@ class NotaViewSet(viewsets.ModelViewSet):
 
     # Generar reporte de alumnos que aprobaron o repobaron cada materia
     @action(detail=False, methods=['get'])
-    def reporte_aprobados(self, request):
+    def reporte_aprobados_reprobados(self, request):
         # Agrupar por alumno y materia, calcular el promedio
-        notas_promedio= Nota.objects.values('alumno', 'materia').annotate(promedio=Avg('nota'))
-        
-        print(notas_promedio)
+        notas_promedio= Nota.objects.values('alumno', 'materia').annotate(
+            count_notas=Count('id'),
+            promedio=Avg('nota')
+        )
         
         resultados = []
         
         # Iterar sobre las notas promedios
         for nota in notas_promedio:
             alumno = Alumno.objects.get(id=nota['alumno'])
-            # nombre_alumno = f"{alumno.nombre} {alumno.apellido}"
             materia = Materia.objects.get(id=nota['materia'])
             promedio = nota['promedio']
+            count_notas = nota['count_notas']
             
+            # ajustar el promedio para que sea entre 3 
+            if count_notas < 3:
+                promedio_ajustado = (promedio * count_notas) / 3
+            else :
+                promedio_ajustado = promedio
+                
             # determinar si el alumno aprueba o reprueba
-            estado = 'Aprobado' if promedio >= 6 else 'Reprobado'
+            estado = 'Aprobado' if promedio_ajustado >= 6 else 'Reprobado'
             
             # agregar el resultado al reporte
             resultados.append({
                 'alumno': f"{alumno.nombre} {alumno.apellido}",
                 'materia': materia.nombre,
-                'promedio': promedio,
+                'promedio': promedio_ajustado.__round__(2),
                 'estado': estado
             })
             
