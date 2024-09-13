a15cc58725c6fc71586a7184e2a11db15d32272a
Ricardo <ricardosolorzano97@gmail.com>
2024-09-13 14:28:27 -0600

    habilitando todos las entradas al servidor en caso que se este usando el puerto asignado al front

--- backend/settings.py
+++ backend/settings.py
@@ -127,6 +127,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-"http://localhost:5173"
-]
+# CORS_ALLOWED_ORIGINS = [
+# "http://localhost:5173"
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
