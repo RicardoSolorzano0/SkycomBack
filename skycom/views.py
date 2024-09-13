9cd4ff76e34d630af01d75d002730d5f264e1428
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-09-13 09:39:43 -0600

    creando el reporte que necesitamos

--- skycom/views.py
+++ skycom/views.py
@@ -1,4 +1,7 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
+from django.db.models import Avg
 from .serializer import ProfesorSerializer, AlumnoSerializer, MateriaSerializer, NotaSerializer
 from .models import Profesor, Alumno, Materia, Nota
 
@@ -20,3 +23,33 @@ class NotaViewSet(viewsets.ModelViewSet):
     queryset = Nota.objects.all()
     serializer_class = NotaSerializer
 
+    # Generar reporte de alumnos que aprobaron o repobaron cada materia
+    @action(detail=False, methods=['get'])
+    def reporte_aprobados(self, request):
+        # Agrupar por alumno y materia, calcular el promedio
+        notas_promedio= Nota.objects.values('alumno', 'materia').annotate(promedio=Avg('nota'))
+        
+        print(notas_promedio)
+        
+        resultados = []
+        
+        # Iterar sobre las notas promedios
+        for nota in notas_promedio:
+            alumno = Alumno.objects.get(id=nota['alumno'])
+            # nombre_alumno = f"{alumno.nombre} {alumno.apellido}"
+            materia = Materia.objects.get(id=nota['materia'])
+            promedio = nota['promedio']
+            
+            # determinar si el alumno aprueba o reprueba
+            estado = 'Aprobado' if promedio >= 6 else 'Reprobado'
+            
+            # agregar el resultado al reporte
+            resultados.append({
+                'alumno': f"{alumno.nombre} {alumno.apellido}",
+                'materia': materia.nombre,
+                'promedio': promedio,
+                'estado': estado
+            })
+            
+        return Response(resultados)
+            
